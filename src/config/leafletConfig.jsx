88741de9908cc7d2c97e26e4ed4f57bf2cf00965
unknown <you@example.com>
2024-04-29 19:18:41 +0200

    Removed leafletConfig.js file

--- src/config/leafletConfig.jsx
+++
@@ -1,9 +0,0 @@
-import L from 'leaflet';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/assets/images/marker-icon-2x.png',
-  iconUrl: '/assets/images/marker-icon.png',
-  shadowUrl: '/assets/images/marker-shadow.png',
-});
