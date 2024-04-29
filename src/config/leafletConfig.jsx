7ed913b520886d50190e9170aa064bf1980a6f28
unknown <you@example.com>
2024-04-29 18:58:26 +0200

    Fixed Leaflet marker images issue in production
    Marker images were copied to the public/images directory. A new Leaflet configuration file (leafletConfig.js) was created to manually set the paths for these images, ensuring they can be found in the production environment. This configuration was imported into the main application file.

--- src/config/leafletConfig.jsx
+++ src/config/leafletConfig.jsx
@@ -2,10 +2,8 @@ import L from 'leaflet';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
-const images = import.meta.globEager('../node_modules/leaflet/dist/images/*.png');
-
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: images['../node_modules/leaflet/dist/images/marker-icon-2x.png'].default,
-  iconUrl: images['../node_modules/leaflet/dist/images/marker-icon.png'].default,
-  shadowUrl: images['../node_modules/leaflet/dist/images/marker-shadow.png'].default,
+  iconRetinaUrl: '/assets/images/marker-icon-2x.png',
+  iconUrl: '/assets/images/marker-icon.png',
+  shadowUrl: '/assets/images/marker-shadow.png',
 });
